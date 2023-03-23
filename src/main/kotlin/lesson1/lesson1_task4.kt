c821a6ab156689893fb43768b11d6a4bc6275503
Azselq <denok2002@gmail.com>
2023-03-24 00:55:03 +0300

    KS-1-4 добавлены 6 переменных и вывод их на экран

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Float = 2.0547945205479453E-4F
+    val apogee: Int = 327000
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
