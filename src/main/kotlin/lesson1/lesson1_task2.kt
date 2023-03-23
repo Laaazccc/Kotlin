4b9d08bde5137c68a45e3f25050dc2316685bd2a
Azselq <denok2002@gmail.com>
2023-03-24 00:42:36 +0300

    KS-1-2 добавлена новая переменная и вывод значений на экран

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package lesson1
+
+fun main() {
+    val countOrders: Int = 75
+    val textThanks: String = "Спасибо за покупку "
+    var countWorkers: Int = 2000
+    println("Всего заказов $countOrders")
+    println(textThanks)
+    //println(countWorkers)
+    countWorkers = 1999
+    println(countWorkers)
+
+}
