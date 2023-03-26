e42e7c54405b458fa98f5d69e4fe54873c0e5dbe
Azselq <denok2002@gmail.com>
2023-03-27 02:10:03 +0300

    KS-1-5 выполнен перевод секунд в минуты и вывод их на экран

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,6 +2,5 @@ package lesson1
 
 fun main() {
     val seconds: Short = 3204
-    val minute = seconds / 60
-    println("$minute:${seconds - minute * 60}")
+    println("${seconds/60}:${seconds%60}")
 }
