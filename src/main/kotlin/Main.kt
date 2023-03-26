4d4d0fecf6608412fe7b1fecdb2656b4b4e5269e
Azselq <denok2002@gmail.com>
2023-03-27 02:46:35 +0300

    KS-2-4 выполнено умножения для нахождения доп рессурсов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(args: Array<String>) {
+    val crystal = 7
+    val iron = 11
+    val buff = 0.2
+
+    val extraCrystal= crystal * buff
+    val extraIron = iron * buff
+
+    println("Добыто дополнительно ${extraCrystal.toInt()} кристалической руды")
+    println("Добыто дополнительно ${extraIron.toInt()} металической руды")
 
 }
